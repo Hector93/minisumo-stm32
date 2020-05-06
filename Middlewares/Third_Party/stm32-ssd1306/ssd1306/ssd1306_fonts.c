ba1982fdbe7e59cae2462f8128bd495db1fa8ba7
Hector93 <hmanuelg93@gmail.com>
2020-05-06 02:42:34 -0500

    se añade libreria ssd1306
    esta libreria es modificada para usar memoria dinamica

---
+++ Middlewares/Third_Party/stm32-ssd1306/ssd1306/ssd1306_fonts.c
@@ -0,0 +1,17 @@
+
+#include "ssd1306_fonts.h"
+#include <stddef.h>
+static const uint16_t* Font7x10 = NULL;
+
+static const uint16_t* Font11x18 = NULL;
+
+static const uint16_t* Font16x26 = NULL;
+
+static const uint16_t* Font6x8 = NULL;
+
+//ESTAS VARIABLES SERAN DEFINIDAS POR EL PROCESO OLED PARA QUE ESTE EN SU STACK
+//EN FORMA DINAMICA
+/* FontDef Font_6x8 = {6,8}; */
+/* FontDef Font_7x10 = {7,10}; */
+/* FontDef Font_11x18 = {11,18}; */
+/* FontDef Font_16x26 = {16,26}; */
